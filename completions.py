515f7a876dd68486fa28f0d0cf6b871a5d8ed988
Sean Millane <jmillaneiv@gmail.com>
2021-01-18 23:17:45 -0500

    changes

--- completions.py
+++ completions.py
@@ -55,7 +55,7 @@ async def on_message(message):
         if puzzles_completed in roles:
             name = roles[puzzles_completed]
             role = message.author.guild.get_role(name)
-            await message.author.add_roles(role=role)
+            await message.author.add_roles(role)
             await message.channel.send('Congrats on the new role!!!')
 
     if message.content.startswith('!completed'):
